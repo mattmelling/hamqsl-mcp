029622d9e902c9ef75ef1af161b9cbaf3e90864c
Matt Melling <mattmelling@fastmail.com>
2026-04-24 22:09:35 +0100

    add http transport option

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ ADD . /opt/hamqsl-mcp
 RUN python3 -m venv /opt/hamqsl-mcp/venv
 RUN /opt/hamqsl-mcp/venv/bin/pip install /opt/hamqsl-mcp
 RUN apt-get clean
-CMD ["/opt/hamqsl-mcp/venv/bin/hamqsl-mcp"]
+CMD ["/opt/hamqsl-mcp/venv/bin/hamqsl-mcp-http"]
--- hamqsl_mcp/__main__.py
+++ hamqsl_mcp/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 from fastmcp import FastMCP
 
 from hamqsl_mcp.hamqsl import mcp as hamqsl_mcp
@@ -8,5 +9,14 @@ mcp.mount(hamqsl_mcp, namespace='hamqsl')
 def main():
     mcp.run()
 
+def main_http():
+    mcp.run(transport="http", host="127.0.0.1", port=3000)
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser('hamqsl-mcp')
+    parser.add_argument('-u', '--http', action='store_true')
+    args = parser.parse_args()
+    if args.http:
+        main_http()
+    else:
+        main()
